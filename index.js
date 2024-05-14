08cabb940fcf78154670ea322607ce1c75b562fe
Mehebul02 <mehebul2123@gmail.com>
2024-05-14 15:11:39 +0600

    Git ignore added

--- index.js
+++ index.js
@@ -22,21 +22,21 @@ app.use(express.json());
 app.use(cookieParser());
 // jwt verify token
 const verifyToken = (req, res, next) => {
-  const token = req.cookies?.token;
-  if (!token) return res.status(401).send({ message: "Unauthorized access" });
+  const token = req.cookies?.token
+  if (!token) return res.status(401).send({ message: 'unauthorized access' })
   if (token) {
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
       if (err) {
-        return res.status(401).send({ message: "Unauthorized access" });
-        // return
+        console.log(err)
+        return res.status(401).send({ message: 'unauthorized access' })
       }
-      console.log(decoded);
-      req.user = decoded;
-      next();
-    });
+      console.log(decoded)
+
+      req.user = decoded
+      next()
+    })
   }
-  console.log(token);
-};
+}
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.po42dna.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -54,32 +54,31 @@ async function run() {
     const submitCollection = client.db("assignmentsDB").collection("submits");
 
     // jwt generate
-    app.post("/jwt", async (req, res) => {
-      const user = req.body;
-      console.log("Dynamic token---->", user);
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: "10h",
-      });
+    app.post('/jwt', async (req, res) => {
+      const email = req.body
+      const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '365d',
+      })
       res
-        .cookie("token", token, {
+        .cookie('token', token, {
           httpOnly: true,
-          secure: process.env.NODE_ENV === "production",
-          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+          secure: process.env.NODE_ENV === 'production',
+          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
         })
-        .send({ success: true });
-    });
+        .send({ success: true })
+    })
 
     // clear token logout
-    app.get("/logout", (req, res) => {
+    app.get('/logout', (req, res) => {
       res
-        .clearCookie("token", {
+        .clearCookie('token', {
           httpOnly: true,
-          secure: process.env.NODE_ENV === "production",
-          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+          secure: process.env.NODE_ENV === 'production',
+          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
           maxAge: 0,
         })
-        .send({ success: true });
-    });
+        .send({ success: true })
+    })
 
     // Assignment spacify data
     app.get("/assignments/:id", async (req, res) => {
@@ -90,10 +89,7 @@ async function run() {
     });
     // data server site get
     app.get("/assignments", async (req, res) => {
-      const filter = req.body.filter;
-      let query = {};
-      if (filter) query = { difficulty: filter };
-      const result = await assignmentsCollection.find(query).toArray();
+      const result = await assignmentsCollection.find().toArray();
       res.send(result);
     });
     // Assignment post
@@ -109,22 +105,27 @@ async function run() {
     res.send(result)
    })
   //  spacify assignment 
-  app.get('/submits/:id',async(req,res)=>{
+  app.get('/submits/:id',verifyToken,async(req,res)=>{
     const id =req.params.id
     const query = {_id : new ObjectId (id)}
     const result =await submitCollection.findOne(query)
     res.send(result)
   })
-    app.post("/submits", async (req, res) => {
+    app.post("/submits",verifyToken, async (req, res) => {
       const submitData = req.body;
       // console.log(bidData)
       const result = await submitCollection.insertOne(submitData);
       res.send(result);
     });
     // my assignment
-    app.get("/submits/:email", async (req, res) => {
+    app.get("/my-submit/:email",verifyToken,async (req, res) => {
+      const tokenEmail = req.user?.email;
+      console.log(tokenEmail, "from token");
       const email = req.params.email;
-      const query = { email };
+      if(tokenEmail === email){
+        return res.status(401).send({ message: "forbidden access" })
+      }
+      const query = { email : email };
       const result = await submitCollection.find(query).toArray();
       res.send(result);
     });
@@ -153,6 +154,19 @@ async function run() {
       console.log("Database delete ", id);
       const query = { _id: new ObjectId(id) };
       const result = await assignmentsCollection.deleteOne(query);
+      res.send(result);
+    });
+    app.put("/update/:id", async (req, res) => {
+      const id = req.params.id;
+      const giveMark = req.body;
+      const query = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          ...giveMark,
+        },
+      };
+      const result = await submitCollection.updateOne(query, updateDoc, options);
       res.send(result);
     });
     // update data
